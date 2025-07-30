38bab3252d313957443244f07231b46326ccfb71
Alexis Collante <141279402+CollanteAlexis@users.noreply.github.com>
2025-07-30 18:57:38 -0500

    tablero1.py

--- tablero1.py
+++ tablero1.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.set_page_config(layout="centered", page_title="Talento_Tech2", page_icon=":smile:")
